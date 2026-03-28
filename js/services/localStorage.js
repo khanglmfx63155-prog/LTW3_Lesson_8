5c37590d15c5a78775688cf38c889230dd47f13e
khanglmfx63155-prog <khanglmfx63155@funix.edu.vn>
2026-03-28 14:01:39 +0700

    fix

--- js/services/localStorage.js
+++ js/services/localStorage.js
@@ -27,6 +27,28 @@ const blog_data = [
     "Món gà KFC thật là ngon! Ăn rất là đã",
     "./asset/kfc.jpg"
   ),
+  new Blog(
+    "Nước Ý thật đẹp",
+    "Nam Cao",
+    "1/2/2004",
+    "Nước Ý là 1 nơi tuyệt vời nhất mà tôi đã từng đật chân tới",
+    "./asset/italy.jpg"
+  ),
+  new Blog(
+    "Nước Pháp thật Lãng mạn",
+    "Văn Cao",
+    "13/12/2024",
+    "Nước Pháp là một đất nước lãng mạn nhất thế giới",
+    "./asset/france.jpg"
+  ),
+
+  new Blog(
+    "Món gà KFC",
+    "Cao Cao",
+    "13/11/2014",
+    "Món gà KFC thật là ngon! Ăn rất là đã",
+    "./asset/kfc.jpg"
+  ),
 ];
 
 // Lay  du lieu blog tu local storage
